efa925064057d0640f24f4593134f29760f32c1a
Wilker Marques <90647686+WilkerMarques@users.noreply.github.com>
2021-10-14 17:35:50 -0400

    Add files via upload

---
+++ crudpadrao/src/main/java/repositorio/CategoriaRepositorio.java
@@ -0,0 +1,37 @@
+package repositorio;
+
+import java.util.List;
+
+import javax.persistence.Query;
+
+import dominio.Categoria;
+
+public class CategoriaRepositorio extends BaseRepositorio {
+
+	public CategoriaRepositorio(String nomeUnidade) {
+		super(nomeUnidade);
+		this.em = this.emf.createEntityManager();
+	}
+
+	//crud
+	
+	public void create() {}
+	
+	public List<Categoria> readAll() {
+		Query qry = this.em.createQuery("FROM Categoria");
+		@SuppressWarnings("unchecked")
+		List<Categoria> lista = qry.getResultList();
+		this.em.close();
+		this.emf.close();
+		return lista;
+	}
+
+	public void read(int id) {}
+	
+	public void update() {}
+	
+	public void delete() {}
+	
+	
+	
+}
